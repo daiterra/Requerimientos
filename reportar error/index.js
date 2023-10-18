0eb40d08fca36d0752b9223654774b04508a5d75
daiterranova <daiana.terranova@gmail.com>
2023-10-18 17:16:57 -0300

    add interaction to open form when user clicks error btn

--- reportar error/index.js
+++ reportar error/index.js
@@ -1,6 +1,8 @@
 const reportDialog = document.getElementById("reportDialog");
-const openDialogBtn = document.getElementById("openDialog");
+const openDialogBtn = document.getElementById("openDialogBtn");
 const closeDialogBtn = document.getElementById("closeDialog");
+const openDialogBtnIcon = document.getElementById("openDialogBtnIcon")
 
 openDialogBtn.addEventListener("click", () => reportDialog.showModal());
+openDialogBtnIcon.addEventListener("click", () => reportDialog.showModal());
 closeDialogBtn.addEventListener("click", () => reportDialog.close());
