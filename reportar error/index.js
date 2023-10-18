87261e453611a6d1ee63ae766341a17ddfbe5490
Federico <fedealvareztorres@gmail.com>
2023-10-18 18:12:29 -0300

    Some fixes

--- reportar error/index.js
+++ reportar error/index.js
@@ -1,8 +1,13 @@
 const reportDialog = document.getElementById("reportDialog");
 const openDialogBtn = document.getElementById("openDialogBtn");
 const closeDialogBtn = document.getElementById("closeDialog");
-const openDialogBtnIcon = document.getElementById("openDialogBtnIcon")
+const openDialogBtnIcon = document.getElementById("openDialogBtnIcon");
 
 openDialogBtn.addEventListener("click", () => reportDialog.showModal());
 openDialogBtnIcon.addEventListener("click", () => reportDialog.showModal());
 closeDialogBtn.addEventListener("click", () => reportDialog.close());
+
+window.addEventListener("scroll", function () {
+  var actionBar = document.getElementById("ActionBar");
+  actionBar.classList.toggle("sticky", window.scrollY > 300);
+});
