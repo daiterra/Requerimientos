3b364cd78f9fb1443a4bf6a73aaea1d0af66d169
daiterranova <daiana.terranova@gmail.com>
2023-11-10 19:15:54 -0300

    capture confirmation container and set logic to display it when the form has been submitted

--- reportar-error/index.js
+++ reportar-error/index.js
@@ -3,11 +3,26 @@ const openDialogBtn = document.getElementById("openDialogBtn");
 const closeDialogBtn = document.getElementById("closeDialog");
 const openDialogBtnIcon = document.getElementById("openDialogBtnIcon")
 const actionBar = document.getElementById("ActionBar");
+const confirmationView = document.getElementById("confirmationView"); 
+const reportForm = document.getElementById('reportForm');
+openDialogBtn.addEventListener("click", () =>{ reportDialog.showModal()});
 
-openDialogBtn.addEventListener("click", () => reportDialog.showModal());
-openDialogBtnIcon.addEventListener("click", () => reportDialog.showModal());
-closeDialogBtn.addEventListener("click", () => reportDialog.close());
+openDialogBtnIcon.addEventListener("click", () => {
+  reportDialog.showModal()           
+  document.getElementById("body").style.overflow = "hidden"
+});
+closeDialogBtn.addEventListener("click", () => {
+  reportDialog.close() 
+  confirmationView.classList.remove('active');
+  reportForm.style.display = 'flex';
+});
 
 window.addEventListener("scroll", function () {
     actionBar.classList.toggle("sticky", window.scrollY > 300);
   });
+
+reportForm.addEventListener('submit', (event) => {
+    event.preventDefault(); 
+    reportForm.style.display = 'none';
+    document.getElementById('confirmationView').classList.add('active');
+});
