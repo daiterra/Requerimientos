655ba23927a5617dd1379232350eefd0859491aa
daiterranova <daiana.terranova@gmail.com>
2024-03-15 20:19:22 -0300

    add styles for cards

--- elecciones/who-is-who-candidates/assets/scripts.js
+++ elecciones/who-is-who-candidates/assets/scripts.js
@@ -19,8 +19,8 @@ const data = [
     {
         party: "Partido de los Trabajadores",
         image: "MASSA.png", // ver de pasarle el link de la imagen de forma dinamica
-        color: "color-first",
-        bg: "bg-first",
+        color: "color-second",
+        bg: "bg-second",
         department: "Artigas",
         firstPosition: {
             name: "Massa",
@@ -35,8 +35,8 @@ const data = [
     {
         party: "Partido Independiente",
         image: "LAVAGNA1.png", // ver de pasarle el link de la imagen de forma dinamica
-        color: "color-first",
-        bg: "bg-first",
+        color: "color-third",
+        bg: "bg-third",
         department: "Artigas",
         firstPosition: {
             name: "R. Lavagna",
@@ -52,8 +52,8 @@ const data = [
     {
         party: "Partido Colorado",
         image: "FERNANDEZ1.png", // ver de pasarle el link de la imagen de forma dinamica
-        color: "color-first",
-        bg: "bg-first",
+        color: "color-fourth",
+        bg: "bg-fourth",
         department: "Montevideo",
         firstPosition: {
             name: "--",
@@ -68,8 +68,8 @@ const data = [
     {
         party: "Partido Colorado",
         image: "FERNANDEZ1.png", // ver de pasarle el link de la imagen de forma dinamica
-        color: "color-first",
-        bg: "bg-first",
+        color: "color-fourth",
+        bg: "bg-fourth",
         department: "Maldonado",
         firstPosition: {
             name: "--",
@@ -180,8 +180,8 @@ const data = [
     {
         party: "Partido Colorado",
         image: "LAVAGNA1.png", // ver de pasarle el link de la imagen de forma dinamica
-        color: "color-first",
-        bg: "bg-first",
+        color: "color-fourth",
+        bg: "bg-fourth",
         department: "Canelones",
         firstPosition: {
             name: "Persona #1",
@@ -332,54 +332,69 @@ function createSlider(parent) {
         slides: glideSlides,
     }
 }
-function createNumberedList(items) {
+function createNumberedList(items, bg) {
     const ol = document.createElement("ol")
     ol.className = "legislative-list"
 
-    items.forEach((item) => {
+    items.forEach((item, index) => {
         const li = document.createElement("li")
         li.innerText = item
+        if (index === 0) {
+            li.classList.add(bg)
+        }
         ol.appendChild(li)
     })
 
     return ol
 }
 function createArticle(params) {
-    const { image, applicants, party, firstPosition, secondPosition } = params
+    const {
+        image,
+        applicants,
+        party,
+        firstPosition,
+        secondPosition,
+        color,
+        bg,
+    } = params
     const article = document.createElement("article")
     article.className = "card-container"
     const partyTag = document.createElement("h3")
-    partyTag.className = "card-title party president"
+    partyTag.className = "card-title party"
     partyTag.innerText = party
+    if (firstPosition.job === "Presidente") {
+        partyTag.classList.add(color)
+        partyTag.style.backgroundColor = "#dfe0e2"
+    } else {
+        partyTag.classList.add(bg)
+    }
     const photoTag = document.createElement("div")
     photoTag.className = "card-image-container"
     photoTag.innerHTML = `<img src="https://elecciones2019.lanacion.com.ar/admin/media/candidatos_fotos/${image}"  alt="${firstPosition} Foto" />`
     article.appendChild(partyTag)
     article.appendChild(photoTag)
     if (applicants.length > 0) {
-        article.appendChild(createNumberedList(applicants))
+        article.appendChild(createNumberedList(applicants, bg))
     } else {
-        article.appendChild(createSpan(firstPosition.name, "name-first"))
         article.appendChild(createSpan(firstPosition.job, "first-position"))
+        article.appendChild(
+            createSpan(firstPosition.name, "name-first", bg, "#dfe0e2")
+        )
         article.appendChild(createSpan("", "divider"))
         article.appendChild(createSpan(secondPosition.job, "second-position"))
         article.appendChild(createSpan(secondPosition.name, "name-second"))
     }
-
-    // let glideItem = document.querySelectorAll(".glide__slide")
-    // console.log("flide", glideItem)
-    // const li = document.createElement("li")
-    // li.className = "glide__slide"
-    // li.appendChild(article)
-    // console.log("article", article)
     return article
 }
 
-function createSpan(content, className) {
+function createSpan(content, className, bgColor, colorText) {
     const span = document.createElement("span")
     span.className = className
     span.innerText = content
-
+    if (bgColor) {
+        span.classList.add(bgColor)
+        span.style.color = colorText
+    }
     return span
 }
 
@@ -388,13 +403,12 @@ function handleDepartmentChange(event) {
     const selectedValue = dropdownMenu.options[event.target.selectedIndex].label
 
     const allDepartments = document.querySelectorAll(".section-container")
-    allDepartments.forEach((department, index) => {
+    allDepartments.forEach((department) => {
         const departmentLabel =
             department.querySelector(".heading-location").textContent
         department.style.display = "none"
 
         if (selectedValue == departmentLabel) {
-            // console.log("index", index)
             department.style.display = "block"
         }
     })
